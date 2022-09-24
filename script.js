fff184f3ace2bf628a0f510fd757245c7dcdbb11
LoredanaBoca <113233024+LoredanaBoca@users.noreply.github.com>
2022-09-24 12:20:46 +0100

    update

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ const {
   core: { test, expect, run },
   prettify
 } = window.jestLite;
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
 
 /* =================
   FIND ELEMENTS
